c88bffb60fc3154a58663c7284da1d3d92e3c8b2
mohamad el cheikh ali <mohammadsheikhali@gmail.com>
2019-03-24 17:02:45 +0200

    change shared perference approach to remove async from getters

--- lib/core/controllers/shared_perferences.dart
+++ lib/core/controllers/shared_perferences.dart
@@ -1,6 +1,23 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-class ProjectSharedPreferences {
+class ProjectSharedPreferences
+{
+  ProjectSharedPreferences._();
+  static final ProjectSharedPreferences instance = ProjectSharedPreferences._();
+  SharedPreferences prefs;
+
+
+  init() async
+  {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  get perfs
+  {
+    if(perfs==null)
+      throw Exception("call init before get perfs");
+    return prefs;
+  }
   ///
   /// Instantiation of the SharedPreferences library
   ///
@@ -10,52 +27,42 @@ class ProjectSharedPreferences {
   /// ------------------------------------------------------------
   /// Method that returns the meter reading is sync with backend
   /// ------------------------------------------------------------
-  static Future<bool> isMeterReadingSync() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
+  bool isMeterReadingSync() {
     return prefs.getBool(_meterReadingSync) ?? false;
   }
 
   /// ----------------------------------------------------------
   /// Method that saves the state of meter reading table
   /// ----------------------------------------------------------
-  static Future<bool> setMeterReadingSync(bool value) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
+  Future<bool> setMeterReadingSync(bool value) async {
     return prefs.setBool(_meterReadingSync, value);
   }
 
   /// ------------------------------------------------------------
   /// Method that returns the meter collection is sync with backend
   /// ------------------------------------------------------------
-  static Future<bool> isCollectionSync() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
+  bool isCollectionSync() {
     return prefs.getBool(_meterCollectionSync) ?? false;
   }
 
   /// ----------------------------------------------------------
   /// Method that saves the state of meter collection table
   /// ----------------------------------------------------------
-  static Future<bool> setCollectionSync(bool value) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
+  Future<bool> setCollectionSync(bool value) async {
     return prefs.setBool(_meterCollectionSync, value);
   }
 
   /// ------------------------------------------------------------
   /// Method that returns database current version
   /// ------------------------------------------------------------
-  static Future<int> getDataBaseVersion() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
+  int getDataBaseVersion() {
     return prefs.getInt(_dataBaseVersion) ?? 1;
   }
 
   /// ----------------------------------------------------------
   /// Method that saves database current version
   /// ----------------------------------------------------------
-  static Future<bool> setDataBaseVersion(int value) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
+  Future<bool> setDataBaseVersion(int value) async {
     return prefs.setInt(_dataBaseVersion, value);
   }
 }
