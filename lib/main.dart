5069fa9253c96efa2d9bbe49f1f7e771bf7b394c
Dakroub <m.dakroub7@gmail.com>
2019-03-24 15:32:19 +0200

    Fixed iOS setup

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:ashal/ui/home/home_page.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:ashal/localization.dart';
 import 'package:flutter/material.dart';
-import 'package:multicast_lock/multicast_lock.dart';
 
 void main() => runApp(MainApp());
 
