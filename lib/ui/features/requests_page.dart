3fba7815840340c1691f7dce79fac2e9b439eb40
Dakroub <m.dakroub7@gmail.com>
2019-04-22 11:19:27 +0300

    Updated starter img

--- lib/ui/features/requests_page.dart
+++ lib/ui/features/requests_page.dart
@@ -1,6 +1,7 @@
 import 'package:ashal/ui/models/card_item.dart';
 import 'package:ashal/ui/models/card_items.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:ashal/ui/theme.dart' as Theme;
 
 class RequestsPage extends StatefulWidget {
 
@@ -13,8 +14,25 @@ class RequestsPage extends StatefulWidget {
 }
 
 class _RequestsPageState extends State<RequestsPage> {
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Container(
+      color: Theme.Colors.cardPageBackground,
+      child: ListView(
+        children: <Widget>[
+          new Center(
+            child: new Hero(
+              tag: 'card-icon-${widget.cardItem.id}',
+              child: new Image(
+                image: new AssetImage(widget.cardItem.image),
+                height: Theme.Dimens.cardHeight,
+                width: Theme.Dimens.cardWidth,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
